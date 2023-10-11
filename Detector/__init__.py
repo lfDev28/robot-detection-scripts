0ad994c73c6a0efa4e70294dcb0e7dfb783dd20f
Lee Forster <lee.forster28@gmail.com>
2023-10-12 10:23:32 +1100

    Composed to one cv2 instance

--- Detector/__init__.py
+++ Detector/__init__.py
@@ -1,11 +1,18 @@
 import cv2
-import numpy as np
 
+class SingletonMeta(type):
+    _instances = {}
 
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            instance = super().__call__(*args, **kwargs)
+            cls._instances[cls] = instance
+        return cls._instances[cls]
 
-class Detector:
+class Detector(metaclass=SingletonMeta):
     def __init__(self, source=0):
-        self.cap = cv2.VideoCapture(source)
+        if not hasattr(self, "cap"):  # Check if cap already exists
+            self.cap = cv2.VideoCapture(source)
 
     def get_frame(self):
         _, frame = self.cap.read()
@@ -19,16 +26,16 @@ class Detector:
         self.cap.release()
         cv2.destroyAllWindows()
 
-        
+    def main_loop(self, detectors):
+        while True:
+            frame = self.get_frame()
+            for detector in detectors:
+                processed_frame = detector.process_frame(frame)
+                detector_name = type(detector).__name__
+                self.show_frame(processed_frame, window_name=detector_name)
 
+            # Break the loop if 'q' is pressed
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
 
-
-
-
-
-    
-
-
-
-
-
+        self.release()
--- app.py
+++ app.py
@@ -1,58 +1,12 @@
 from Detector.Feature import Feature
-from Detector.SignDetector import SignDetector
 from Detector.EdgeDetector import EdgeDetector
-from helpers import close_screen_capture
-import cv2
-import numpy as np
-
-
-
-def feature():
-    feature_detector = Feature(source=0)
-
-    try:
-        while True:
-            frame = feature_detector.get_frame()
-            detected_frame = feature_detector.detect_feature(frame, "yellow", "circle")
-            feature_detector.show_frame(detected_frame)
-            
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                cv2.destroyAllWindows()
-                break
-
-    except KeyboardInterrupt:
-        pass
-    finally:
-        feature_detector.release()
-
-
-def sign_detector():
-    sign_detector = SignDetector()
-    try:
-        while True:
-            frame = sign_detector.get_frame()
-            detected_frame = sign_detector.detect_sign(frame)
-            sign_detector.show_frame(detected_frame)
-            # Break the loop if 'q' is pressed
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                cv2.destroyAllWindows()
-                break
-    except KeyboardInterrupt:
-        pass
-    finally:
-        sign_detector.release()
-
-
-def edge_detector():
-    edge_detector = EdgeDetector()
-    edge_detector.process()
-
-
+from Detector.SignDetector import SignDetector
+from Detector import Detector
 
 if __name__ == '__main__':
-    edge_detector()
+    edge_detector = EdgeDetector()
+    feature_detector = Feature()
+    sign_detector = SignDetector()
 
-        
-
-
-
+    main_detector = Detector()
+    main_detector.main_loop([edge_detector, feature_detector, sign_detector])
